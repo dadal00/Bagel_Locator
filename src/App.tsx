b99783ffd4d01a44d2e83c53addf7807eadc5cff
dadal00 <dylanadal.00@gmail.com>
2024-06-23 15:06:23 -0700

    no profile

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { TouchableOpacity, Image, Dimensions } from 'react-native';
 import HomeScreen from './HomeScreen';
 import FinderScreen from './FinderScreen';
 import OrderScreen from './OrderScreen';
-import ProfileScreen from './ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -32,9 +31,6 @@ const App: React.FC = () => {
               case 'Order':
                 iconName = 'cart';
                 break;
-              case 'Profile':
-                iconName = 'profile';
-                break;
               default:
                 iconName = 'bagel';
                 break;
@@ -59,7 +55,6 @@ const App: React.FC = () => {
         <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
         <Tab.Screen name="Finder" component={FinderScreen} options={{ headerShown: false }}/>
         <Tab.Screen name="Order" component={OrderScreen} options={{ headerShown: false }}/>
-        <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -79,7 +74,7 @@ const TabIcon: React.FC<{ iconName: any; routeName: string }> = ({ iconName, rou
       alignItems: 'center',
     }}>
       <Image source={{uri:iconName}} style={{ 
-        width: MAX_ICON_SIZE,
+        width: MAX_ICON_SIZE + 20,
         height: MAX_ICON_SIZE,
         resizeMode: 'contain', }} />
     </TouchableOpacity>
